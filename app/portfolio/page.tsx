5d2f83fc81f221956d1205670e1bea664cf16ba3
Andrew Sameh Adel <andrew.sameh.adel@gmail.com>
2025-08-07 18:57:04 +0300

    [bug] fix type mismatch
    - reconstructed JSON format for project.json to accomodate type
    - redid rendering for changed types
    - removed wrong import

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -2,7 +2,6 @@ import About from "@/app/components/portfolio/About";
 import Contact from "@/app/components/portfolio/Contact";
 import Project from "@/app/components/portfolio/Project";
 import Skill from "@/app/components/portfolio/Skill";
-import Certificate from "@/app/components/portfolio/Certificate";
 
 export default function Page() {
     return (
