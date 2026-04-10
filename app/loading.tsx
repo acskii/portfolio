8915714a9239e13e9dff595b2b057d4a0bfb5760
Andrew Sameh Adel <andrew.sameh.adel@gmail.com>
2026-04-10 16:31:07 +0200

    [feat] add loading

---
+++ app/loading.tsx
@@ -0,0 +1,13 @@
+import Spinner from "./components/LoadingSpinner";
+
+/* Font imports */
+import { poppins } from "@/app/components/fonts";
+
+export default function Loading() {
+    return (
+        <div className="flex flex-col gap-2 justify-center items-center">
+            <Spinner />
+            <span className={`${poppins.className} font-bold text-2xl text-yellow-500 dark:text-white`}>Loading Content...</span>
+        </div>
+    );
+}
