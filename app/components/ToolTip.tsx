c35b9d21bbfc0011ae6066d9750b6b7200904b82
Andrew Sameh Adel <andrew.sameh.adel@gmail.com>
2026-04-10 21:34:07 +0200

    [fix] fix tooltip theme

--- app/components/ToolTip.tsx
+++ app/components/ToolTip.tsx
@@ -5,7 +5,7 @@ export default function Tooltip({ children, text }: { children: ReactNode, text:
     return (
         <div role="tooltip" id={`tootip-${text}`} className="relative group">
             {children}
-            <div className="absolute left-1/2 transform -translate-x-1/2 bottom-full mb-2 hidden group-hover:block bg-amber-500 text-white text-sm font-semibold px-2 z-10">
+            <div className="absolute left-1/2 transform -translate-x-1/2 bottom-full mb-2 hidden group-hover:block bg-amber-500 dark:bg-blue-600 text-white text-sm font-semibold px-2 z-10">
                 {text}
             </div>
         </div>
