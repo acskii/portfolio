68d47bc7458ef5bd134849d20018c4d5fa2412c2
Andrew Sameh Adel <andrew.sameh.adel@gmail.com>
2026-04-11 15:46:17 +0200

    [feat] update navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,15 +1,17 @@
 'use client';
 
 import { useState } from 'react';
+
 import Link from 'next/link';
-import { poppins } from "@/app/components/fonts";
+
+import { poppins } from "@/app/fonts";
 
 const navItems = [
-    { href: "#about", label: "About" },
-    { href: "#contact", label: "Contact" },
-    { href: "#projects", label: "Projects" },
-    { href: "#skills", label: "Skills" },
-    { href: "#certificates", label: "Certificates" },
+    { href: "/#about", label: "About" },
+    { href: "/#contact", label: "Contact" },
+    { href: "/resume", label: "Resume" },
+    { href: "/projects", label: "Projects" },
+    { href: "/certificates", label: "Certificates" },
 ];
 
 export default function NavBar() {
@@ -18,14 +20,18 @@ export default function NavBar() {
     return (
         <nav className={`${poppins.className} max-w-screen bg-yellow-400 dark:bg-indigo-950 min-h-20 flex flex-row justify-between items-center px-8 py-5 relative`}>
             <div className="flex flex-row gap-1 items-center">
-                <img src="../../../favicon.ico" alt="king_cat.ico" className="w-[40px] md:w-[50px]" />
-                <h1 className="text-white text-2xl md:text-3xl font-bold">Portfolio</h1>
+                <div className="mr-4 border-4 border-black dark:border-white p-1 bg-yellow-500 dark:bg-blue-600">
+                    <img src="../../../favicon.ico" alt="king_cat.ico" className="w-[40px] md:w-[60px]" />
+                </div>
+                <span className={`${poppins.className} text-4xl font-black dark:text-white uppercase tracking-tighter`}>
+                    Portfolio
+                </span>
             </div>
 
             <div className="hidden md:flex flex-row gap-5 px-4 py-1 text-xl text-white font-semibold">
                 {navItems.map(({ href, label }) => (
                     <Link key={label} href={href} scroll={true}>
-                        <h3 className="hover:text-amber-600 hover:cursor-pointer">
+                        <h3 className="hover:text-amber-600 cursor-pointer dark:hover:text-blue-600">
                             {label}
                         </h3>
                     </Link>
@@ -63,10 +69,10 @@ export default function NavBar() {
                             href={href}
                             scroll={true}
                             onClick={() => setMenuOpen(false)}
-                            className="px-4 py-2 text-lg hover:text-amber-600 hover:bg-yellow-500"
+                            className="px-4 py-2 text-lg hover:text-amber-600 hover:bg-yellow-500 dark:hover:text-blue-500 dark:hover:bg-violet-700"
                         >
                             {label}
-                            <hr className="my-2 h-0.5 border-t-0 bg-amber-200" />
+                            <hr className="my-2 h-0.5 border-t-0 bg-amber-200 dark:bg-violet-500" />
                         </Link>
                     ))}
                 </div>
